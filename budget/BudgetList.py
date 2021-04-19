3d868e4f3b6f23b7f3f53980d94ee41910635d43
ManishV18 <82750764+ManishV18@users.noreply.github.com>
2021-04-19 14:31:57 +0530

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -22,15 +22,16 @@ class BudgetList:
         return (len(self.expenses) + len(self.overages))
 
     def __iter__(self):
-        self.iter_e=iter(self.expenses)
-        self.iter_o=iter(self.overages)
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
         return self
 
     def __next__ (self):
         try:
-            return next(self.iter_e)
+            return self.iter_e.__next__()
         except StopIteration as stop:
-            return next(self.iter_o)
+            return self.iter_o.__next__()
+
 
 def main():
     myBudgetList=BudgetList(1200)
